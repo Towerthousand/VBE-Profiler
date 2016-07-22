af73f95435f5b69b4913f00e4b8a5a00ffeb5603
Towerthousand <dani.torramilans@gmail.com>
2016-07-22 17:57:18 +0200

    Fixed a few timings, tabs->spaces

--- include/VBE-Profiler/profiler/Profiler.hpp
+++ include/VBE-Profiler/profiler/Profiler.hpp
@@ -8,109 +8,110 @@
 
 class DeferredContainer;
 class Profiler : public GameObject {
-	public:
-		Profiler();
-		Profiler(std::string vertShader, std::string fragShader);
-		~Profiler();
+    public:
+        Profiler();
+        Profiler(std::string vertShader, std::string fragShader);
+        ~Profiler();
 
-		static void pushMark(const std::string& name, const std::string& definition);
-		static void popMark();
-		static bool isShown();
-		static void setShown(bool shown);
-		static bool isLogShown();
-		static void setShowLog(bool shown);
-		static bool isTimeShown();
-		static void setShowTime(bool shown);
+        static void pushMark(const std::string& name, const std::string& definition);
+        static void popMark();
+        static bool isShown();
+        static void setShown(bool shown);
+        static bool isLogShown();
+        static void setShowLog(bool shown);
+        static bool isTimeShown();
+        static void setShowTime(bool shown);
 
-	protected:
-		virtual void render(ImDrawList** const cmd_lists, int cmd_lists_count) const;
-		virtual void renderCustomInterface() const;
+    protected:
+        virtual void render(ImDrawList** const cmd_lists, int cmd_lists_count) const;
+        virtual void renderCustomInterface() const;
 
-	private:
-		struct Node {
-				Node() {start();}
-				Node(const std::string& name, const std::string& desc, Node* parent)
-					: parent(parent), name(name), desc(desc) {start();}
-				~Node() {}
+    private:
+        struct Node {
+                Node() {start();}
+                Node(const std::string& name, const std::string& desc, Node* parent)
+                    : parent(parent), name(name), desc(desc) {start();}
+                ~Node() {}
 
-				float getTime() const {return totalTime;}
-				void start() {timeStart = Clock::getSeconds();}
-				void stop() {timeEnd = Clock::getSeconds(); totalTime += timeEnd-timeStart; timeStart = 0.0f;}
+                float getTime() const {return totalTime;}
+                void start() {timeStart = Clock::getSeconds();}
+                void stop() {timeEnd = Clock::getSeconds(); totalTime += timeEnd-timeStart; timeStart = 0.0f;}
 
-				Node* parent = nullptr;
-				std::list<Node> children;
-				std::string name = std::string("invalid");
-				std::string desc = std::string("invalid");
-			private:
-				float totalTime = 0.0f;
-				float timeStart = 0.0f;
-				float timeEnd = 0.0f;
-		};
+                Node* parent = nullptr;
+                std::list<Node> children;
+                std::string name = std::string("invalid");
+                std::string desc = std::string("invalid");
+            private:
+                float totalTime = 0.0f;
+                float timeStart = 0.0f;
+                float timeEnd = 0.0f;
+        };
 
-		class Watcher final : public GameObject {
-			public:
-				Watcher();
-				~Watcher();
+        class Watcher final : public GameObject {
+            public:
+                Watcher();
+                ~Watcher();
 
-				void fixedUpdate(float deltaTime);
-				void update(float deltaTime);
-				void draw() const;
-		};
+                void fixedUpdate(float deltaTime);
+                void update(float deltaTime);
+                void draw() const;
+        };
 
-		struct Historial final {
-				Historial(unsigned long int id) : id(id) {}
-				const unsigned long int id = 0;
-				float current = 0.0f;
-				float past[PROFILER_HIST_SIZE];
-		};
+        struct Historial final {
+                Historial(unsigned long int id) : id(id) {}
+                const unsigned long int id = 0;
+                float current = 0.0f;
+                float past[PROFILER_HIST_SIZE];
+        };
 
-		static void renderHandle(ImDrawList** const cmd_lists, int cmd_lists_count);
-		static const char*getClipHandle();
-		static void setClipHandle(const char* text);
+        static void renderHandle(ImDrawList** const cmd_lists, int cmd_lists_count);
+        static const char*getClipHandle();
+        static void setClipHandle(const char* text);
 
-		//callback
-		const char* getClip() const;
-		//callback
-		void setClip(const char* text) const;
+        //callback
+        const char* getClip() const;
+        //callback
+        void setClip(const char* text) const;
 
-		void fixedUpdate(float deltaTime) final override;
-		void update(float deltaTime) final override;
-		void draw() const final override;
-		void processNodeAverage(const Node& n);
-		void resetTreeDraw() const;
-		void resetTreeUpdate() const;
-		void resetTreeFixed() const;
-		void resetTreeSwap() const;
-		void setImguiIO(float deltaTime) const;
-		void timeWindow() const;
-		void logWindow() const;
-		void uiProcessNode(const Node& n) const;
+        void fixedUpdate(float deltaTime) final override;
+        void update(float deltaTime) final override;
+        void draw() const final override;
+        void processNodeAverage(const Node& n);
+        void resetTreeDraw() const;
+        void resetTreeUpdate() const;
+        void resetTreeFixed() const;
+        void resetTreeSwap() const;
+        void resetTreeWhole() const;
+        void setImguiIO(float deltaTime) const;
+        void timeWindow() const;
+        void logWindow() const;
+        void uiProcessNode(const Node& n) const;
 
-		static Profiler* instance;
-		static std::string defaultVS;
-		static std::string defaultFS;
+        static Profiler* instance;
+        static std::string defaultVS;
+        static std::string defaultFS;
 
-		int timeAvgOffset = -1;
-		mutable int frameCount = 0;
-		mutable float timePassed = 0.0f;
-		mutable int FPS = 0;
-		bool showProfiler = false;
-		bool showTime = true;
-		bool showLog = true;
-		float sampleRate = 0.5f;
-		float windowAlpha = 0.9f;
-		vec2ui wsize = vec2ui(0,0);
-		mutable Node treeUpdate;
-		mutable Node treeDraw;
-		mutable Node treeFixed;
-		mutable Node treeSwap;
-		mutable Node treeWhole;
-		mutable Node* currentNode = nullptr;
-		std::map<std::string, Historial> hist;
-		mutable Mesh model;
-		Texture2D tex;
-		ShaderProgram program;
-		mutable std::string clip = "";
+        int timeAvgOffset = -1;
+        mutable int frameCount = 0;
+        mutable float timePassed = 0.0f;
+        mutable int FPS = 0;
+        bool showProfiler = false;
+        bool showTime = true;
+        bool showLog = true;
+        float sampleRate = 0.5f;
+        float windowAlpha = 0.9f;
+        vec2ui wsize = vec2ui(0,0);
+        mutable Node treeUpdate;
+        mutable Node treeDraw;
+        mutable Node treeFixed;
+        mutable Node treeSwap;
+        mutable Node treeWhole;
+        mutable Node* currentNode = nullptr;
+        std::map<std::string, Historial> hist;
+        mutable Mesh model;
+        Texture2D tex;
+        ShaderProgram program;
+        mutable std::string clip = "";
 };
 
 #endif // PROFILER_HPP
